12dca21fad21f043dde2656b203392042002e663
Smognus <miketcoulson@gmail.com>
2015-10-16 14:13:22 -0700

    Fixed the merge issue where `invert' setting was reintroduced.  Added start & stop hour configuration for the second hand.

--- variable-hands-config/js/variable-hands.js
+++ variable-hands-config/js/variable-hands.js
@@ -1,19 +1,24 @@
-
 function getConfigData() {
     var tickSetting = document.getElementById('tick_setting_slider');
     var daySetting = document.getElementById('day_setting_slider');
-    var invertSetting = document.getElementById('invert_setting_slider');
+    var batterySetting = document.getElementById('battery_setting_slider');
+    var secondStartSetting = document.getElementById('second_hand_start');
+    var secondEndSetting = document.getElementById('second_hand_end');
 
     var options = {
 	'tickSetting': tick_setting_slider.checked,
 	'daySetting': day_setting_slider.checked,
-	'invertSetting': invert_setting_slider.checked 
+	'batterySetting': battery_setting_slider.checked,
+	'secondStartSetting': second_hand_start.value,
+	'secondEndSetting': second_hand_end.value
     };
 
     // Save for next launch
     localStorage['tick_setting'] = options['tickSetting'];
     localStorage['day_setting'] = options['daySetting'];
-    localStorage['invert_setting'] = options['invertSetting'];
+    localStorage['battery_setting'] = options['batterySetting'];
+    localStorage['second_hand_start_setting'] = options['secondStartSetting'];
+    localStorage['second_hand_end_setting'] = options['secondEndSetting'];
 
     console.log('Got options: ' + JSON.stringify(options));
     return options;
@@ -43,13 +48,21 @@ submitButton.addEventListener('click', function() {
 (function() {
     var tickSetting = document.getElementById('tick_setting_slider');
     var daySetting = document.getElementById('day_setting_slider');
-    var invertSetting = document.getElementById('invert_setting_slider');
+    var batterySetting = document.getElementById('battery_setting_slider');
+    var secondStartSettingSlider = document.getElementById('second_hand_start_actual_slider');
+    var secondEndSettingSlider = document.getElementById('second_hand_end_actual_slider');
+    var secondStartSettingText = document.getElementById('second_hand_start');
+    var secondEndSettingText = document.getElementById('second_hand_end');
 
     // Load any previously saved configuration, if available
     if(localStorage['tick_setting']) {
 	tickSetting.checked = JSON.parse(localStorage['tick_setting']);
 	daySetting.checked = JSON.parse(localStorage['day_setting']);
-	invertSetting.checked = JSON.parse(localStorage['invert_setting']);
+	batterySetting.checked = JSON.parse(localStorage['battery_setting']);
+	secondStartSettingSlider.value = JSON.parse(localStorage['second_hand_start_setting']);
+	secondEndSettingSlider.value = JSON.parse(localStorage['second_hand_end_setting']);
+	secondStartSettingText.value = JSON.parse(localStorage['second_hand_start_setting']);
+	secondEndSettingText.value = JSON.parse(localStorage['second_hand_end_setting']);
     }
 })();
 
